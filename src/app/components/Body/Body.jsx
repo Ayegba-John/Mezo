d0a232f35eabe135790153187c65af0d27eaa1b1
Ayegba John <ayegbajohn@gmail.com>
2024-12-15 23:57:33 +0100

    paragaph added

--- src/app/components/Body/Body.jsx
+++ src/app/components/Body/Body.jsx
@@ -14,6 +14,13 @@ const body = () => {
           we are ready to ove to the next level
         </div>
         <div className={styels.body_three}>we have made the move</div>
+        <div>
+          The Only Cryptocurrency Exchange You Need Our trading platform has
+          zero fees, instant payment feature, device authorization for added
+          security, allows you to communicate 24/7 with our exceptional customer
+          support, has an incomparable help center that answers all your
+          questions, and real-time price update. What’s not to love?
+        </div>
       </div>
     </div>
   );
