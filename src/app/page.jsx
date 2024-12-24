b514e59f07739e5881891d72b50f698b77927e7d
Ayegba John <ayegbajohn@gmail.com>
2024-12-24 23:50:29 +0100

    Feat:Trade section added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Body from "./components/Body/Body";
 import Product from "./components/Product/Product";
+import Trade from "./components/Trade/Trade";
 
 const page = () => {
   return (
@@ -13,6 +14,7 @@ const page = () => {
       <Product name="iphone" discription="iphone 13" price={1500} />
       <Product name="water" discription="bottle water" price={200} />
       <Product name="fan" discription="Lontor" price={5300} />
+      <Trade />
     </div>
   );
 };
