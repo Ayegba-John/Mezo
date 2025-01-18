dd6b1e1d62a82570e4fce96accd401a76c2ee058
Ayegba John <ayegbajohn@gmail.com>
2025-01-18 23:55:27 +0100

    feat:moior text added

--- src/app/components/Trade/Trade.jsx
+++ src/app/components/Trade/Trade.jsx
@@ -5,6 +5,18 @@ const trade = () => {
     <div className={styles.main}>
       <div className={styles.Wrapper}>
         <div>
+          <div>
+            <p>
+              <span>Who we are </span>
+              <br />
+              At Mezovest, we are on a mission to revolutionize the commerce
+              landscape, and our focus is squarely on the dynamic energy
+              industry, specifically LPG aka cooking gas and CNG . As a leading
+              commerce solution company, we are dedicated to providing
+              innovative, efficient, and seamless solutions that cater to the
+              unique needs of businesses operating in the energy sector.
+            </p>
+          </div>
           <img
             className={styles.group}
             src="swad.svg"
