5b6178d7146b73f9c84ecb5ce51861edcc7a8d73
Ayegba John <ayegbajohn@gmail.com>
2025-02-11 23:36:18 +0100

    feat:minor

--- src/app/components/Section/Section.jsx
+++ src/app/components/Section/Section.jsx
@@ -42,6 +42,17 @@ const section = () => {
         their business as well as several stakeholders in the LPG value chain.
         Mezo Energy Image
       </p>
+      <div>
+        <p>
+          Pandar is a registered trademark of Pandar Resources Ltd with RC
+          1711476 as a duly registered legal entity in Nigeria, with the
+          Nigerian Corporate Affairs Commission. Any unauthorized redistribution
+          or reproduction of any copyrighted materials on this website is
+          strictly prohibited. Other product and company names are trademarks of
+          their respective owners. 1309 Coffeen Avenue STE 1200, Sheridan,
+          Wyoming, 82801.
+        </p>
+      </div>
     </div>
   );
 };
