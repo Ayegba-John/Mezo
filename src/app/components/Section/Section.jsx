dbbb89008b31b791c3b2139125dbea812a3d38c8
Ayegba John <ayegbajohn@gmail.com>
2025-02-18 22:10:00 +0100

    minor

--- src/app/components/Section/Section.jsx
+++ src/app/components/Section/Section.jsx
@@ -25,34 +25,6 @@ const section = () => {
         their business as well as several stakeholders in the LPG value chain.
         Mezo Energy Image
       </p>
-
-      <p>
-        Biggest players in the LPG sector Mezo Energy Trading Limited (METL)
-        Mezoenergy trading limited is an energy company that has a big
-        reputation in gas trading with supply chains across the country. The
-        company was established in 2021 and is positioned to be the fastest
-        growing gas trading company in Nigeria. We have an unmatched reputation
-        for our commitment to our excellence and dedication to quality, safety
-        and use of technology makes us the most reliable partner for most
-        businesses. With our beautiful and innovative composite gas cylinder, we
-        have also been able to win the trust of end-users who share the same
-        core values as we do. In partnership with mezovest, we provide gas
-        financing where we deliver high quality cooking gas to our vendors.
-        Hundreds of small businesses have leveraged this credit system to grow
-        their business as well as several stakeholders in the LPG value chain.
-        Mezo Energy Image
-      </p>
-      <div>
-        <p>
-          Pandar is a registered trademark of Pandar Resources Ltd with RC
-          1711476 as a duly registered legal entity in Nigeria, with the
-          Nigerian Corporate Affairs Commission. Any unauthorized redistribution
-          or reproduction of any copyrighted materials on this website is
-          strictly prohibited. Other product and company names are trademarks of
-          their respective owners. 1309 Coffeen Avenue STE 1200, Sheridan,
-          Wyoming, 82801.
-        </p>
-      </div>
     </div>
   );
 };
