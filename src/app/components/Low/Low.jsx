fa3db45a8a5ce6534d862de39a3253bc258cdc34
Ayegba John <ayegbajohn@gmail.com>
2025-02-21 23:05:23 +0100

    feat:minor

--- src/app/components/Low/Low.jsx
+++ src/app/components/Low/Low.jsx
@@ -4,16 +4,17 @@ const Low = () => {
   return (
     <div className={styles.main}>
       <div className={styles.wrapper}>
-        <div>
+        <div className={styles.wrapper_up}>
           <p className={styles.Low_top}>
             Mezovest offers the best products for a remarkable
           </p>
         </div>
-        <div>
-          <h3>Commerce Experience</h3>
-          <p>
+        <div className={styles.Low_down}>
+          <h3 className={styles.text}>Commerce Experience</h3>
+          <p className={styles.text_low}>
             Stay Ahead of the Competition with Our State-of-the-Art Commerce
-            Solutions. Empower Your Brand with Our End-to-End Commerce Products.
+            Solutions. <br /> Empower Your Brand with Our End-to-End Commerce
+            Products.
           </p>
         </div>
       </div>
